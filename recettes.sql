ea46efba9c7f7ae10193e9d832fcf5d84b6c3623
Bauduin David <davidbauduin1@gmail.com>
2024-04-19 14:13:40 +0200

    new database

--- recettes.sql
+++ recettes.sql
@@ -2,41 +2,51 @@
 CREATE DATABASE IF NOT EXISTS `05042024`;
 USE `05042024`;
 
--- Création de la table recipes
-CREATE TABLE IF NOT EXISTS `recipes` (
-    `recipe_id` int(11) NOT NULL AUTO_INCREMENT,
-    `title` varchar(128) NOT NULL,
-    `recipe` TEXT NOT NULL,
-    `author` varchar(255) NOT NULL,
-    `is_enabled` BOOLEAN NOT NULL,
-    PRIMARY KEY (`recipe_id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Création de la table users
-CREATE TABLE IF NOT EXISTS `users` (
-    `user_id` int(11) NOT NULL AUTO_INCREMENT,
-    `full_name` varchar(64) NOT NULL,
-    `email` varchar(255) NOT NULL,
-    `password` varchar(255) NOT NULL,
-    `age` INT NOT NULL,
-    PRIMARY KEY (`user_id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `recipes`(
+  `recipe_id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `recipe` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `is_enabled` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Suppression des données existantes dans la table `users`
-DELETE FROM `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(64) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Insertion des nouvelles données dans la table `users`
-INSERT INTO `users` (`full_name`, `email`, `age`, `password`) VALUES 
-('Lucas Dupont', 'lucas.dupont@exemple.com', 34, 'test'),
-('Sophie Lefevre', 'sophie.lefevre@exemple.com', 34, 'test'),
-('Maxime Bernard', 'maxime.bernard@exemple.com', 28, 'test');
+INSERT INTO `users` (`id`, `full_name`, `email`, `password`, `age`) VALUES
+(1, 'Lucas Dupont', 'lucas.dupont@exemple.com', 'test', 34),
+(2, 'Sophie Lefevre', 'sophie.lefevre@exemple.com', 'test', 34),
+(3, 'Maxime Bernard', 'maxime.bernard@exemple.com', 'test', 28),
+(5, 'David Bauduin', 'abo@lav.be', 'test', 27);
 
--- Suppression des données existantes dans la table `recipes`
-DELETE FROM `recipes`;
+INSERT INTO `recipes` (`recipe_id`, `title`, `recipe`, `user_id`, `is_enabled`) VALUES
+(1, 'Cassoulet', 'Le cassoulet est une spécialité régionale du Languedoc, à base de haricots secs, généralement blancs, et de viande. À son origine, il était à base de fèves. Le cassoulet tient son nom de la cassole en terre cuite émaillée dite caçòla1 en occitan et fabriquée à Issel.\n', 1, 1),
+(2, 'Couscous', 'Le couscous est d\'une part une semoule de blé dur préparée à l\'huile d\'olive (un des aliments de base traditionnel de la cuisine des pays du Maghreb) et d\'autre part, une spécialité culinaire issue de la cuisine berbère, à base de couscous, de légumes, d\'épices, d\'huile d\'olive et de viande (rouge ou de volaille) ou de poisson.', 1, 0),
+(3, 'Salade Romaine', 'La salade César est une recette de cuisine de salade composée de la cuisine américaine, traditionnellement préparée en salle à côté de la table, à base de laitue romaine, œuf dur, croûtons, parmesan et de « sauce César » à base de parmesan râpé, huile d\'olive, pâte d\'anchois, ail, vinaigre de vin, moutarde, jaune d\'œuf et sauce Worcestershire.', 2, 0),
+(4, 'Escalope milanaise', 'L\'escalope à la milanaise, ou escalope milanaise est une escalope panée, de viande de veau, traditionnellement prise dans le faux-filet. Historiquement, on la cuit avec du beurre. Elle est généralement servie avec salade ou frites, accompagnée de sauce mayonnaise. On peut y ajouter un filet de jus de citron.\n\nEn Italie, ce mets ne se sert pas avec des pâtes.', 3, 1);
 
--- Insertion des nouvelles données dans la table `recipes`
-INSERT INTO `recipes` (`author`, `is_enabled`, `recipe`, `title`) VALUES 
-('lucas.dupont@exemple.com', 1, "Le cassoulet est une spécialité régionale du Languedoc, à base de haricots secs, généralement blancs, et de viande. À son origine, il était à base de fèves. Le cassoulet tient son nom de la cassole en terre cuite émaillée dite caçòla1 en occitan et fabriquée à Issel.\n", 'Cassoulet'),
-('lucas.dupont@exemple.com', 0, "Le couscous est d'une part une semoule de blé dur préparée à l'huile d'olive (un des aliments de base traditionnel de la cuisine des pays du Maghreb) et d'autre part, une spécialité culinaire issue de la cuisine berbère, à base de couscous, de légumes, d'épices, d'huile d'olive et de viande (rouge ou de volaille) ou de poisson.", 'Couscous'),
-('sophie.lefevre@exemple.com', 0, "La salade César est une recette de cuisine de salade composée de la cuisine américaine, traditionnellement préparée en salle à côté de la table, à base de laitue romaine, œuf dur, croûtons, parmesan et de « sauce César » à base de parmesan râpé, huile d'olive, pâte d'anchois, ail, vinaigre de vin, moutarde, jaune d'œuf et sauce Worcestershire.", 'Salade Romaine'),
-('maxime.bernard@exemple.com', 1, "L'escalope à la milanaise, ou escalope milanaise est une escalope panée, de viande de veau, traditionnellement prise dans le faux-filet. Historiquement, on la cuit avec du beurre. Elle est généralement servie avec salade ou frites, accompagnée de sauce mayonnaise. On peut y ajouter un filet de jus de citron.\n\nEn Italie, ce mets ne se sert pas avec des pâtes.", 'Escalope milanaise');
+ALTER TABLE `recipes` ADD PRIMARY KEY (`recipe_id`);
+ALTER TABLE `users` ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `recipes` MODIFY `recipe_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+ALTER TABLE `recipes` ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+
+COMMIT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
